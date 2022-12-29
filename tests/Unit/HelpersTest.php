5f721faef324dc7c96b67a32fc4d5bba0bbb5df1
Tinashe Musonza <tinashemusonza@gmail.com>
2022-12-29 11:23:59 -0500

    wip

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Musonza\LaravelDynamodbChat\Tests\Unit;
+
+use Musonza\LaravelDynamodbChat\Helpers\Helpers;
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    public function testDirectConversationKey()
+    {
+        $id1 = 'id1';
+        $id2 = 'id2';
+        $key1 = Helpers::directConversationKey($id1, $id2);
+        $key2 = Helpers::directConversationKey($id2, $id1);
+
+        $this->assertEquals($key1, $key2);
+        $this->assertEquals("DIRECT#P1#id1P2#id2", $key1);
+    }
+}
